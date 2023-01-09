41ac0a8b8f27dcd940a72570ed4bf5813624bc8a
sandyrodriguesjs <122131843+sandyrodriguesjs@users.noreply.github.com>
2023-01-08 22:06:34 -0300

    Add files via upload

---
+++ des1.py
@@ -0,0 +1,12 @@
+nome = input('Qual é seu nome?')
+print('Seja bem vindo '+ nome +',prazer em te conhecer!')
+
+dia = input('que dia você nasceu?')
+mes = input('que mês em que você nasceu?')
+ano = input('que ano em que vocÊ nasceu?')
+
+print('Voce nasceu no dia',dia,'de',mes,'de',ano)
+a = float(input('Informe um numero:'))
+b = float(input('Informe outro numero:'))
+soma = float(a + b)
+print('A soma dos numeros é {.2}' .format(soma))
